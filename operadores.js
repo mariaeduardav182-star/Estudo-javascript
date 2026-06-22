939eaa1b762beb565fa4a166297231f9dcf1a77e
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-06-22 11:55:22 -0300

    aula1_javascript

---
+++ operadores.js
@@ -0,0 +1,12 @@
+//variaveis
+let a = 5;
+let b = 2;
+
+//operadores matematicos
+let soma = a+b;
+let subtracao = a-b;
+let multiplicao = a*b;
+let divisao = a/b;
+let resto = a%b; 
+
+//operadores de comparação
---
+++ variaveis.js
@@ -0,0 +1,12 @@
+let nome = "Bruno";// Variável do tipo string
+let idade = 39;// Variável do tipo number
+let altura = 1.75;// Variável do tipo number float
+let ativo = true; // Variável do tipo boleano
+let sobrenome = null; //nulo
+let endereco;//indefinido
+let hobbies = ["jogar videogame","assistir séries"]//array
+let pessoa = {nome:"Bruno",idade:"39",altura:1.75}
+
+console.log("Olá,"+nome);
+console.log("idade:"+idade);
+console.log("altura:"+altura);
